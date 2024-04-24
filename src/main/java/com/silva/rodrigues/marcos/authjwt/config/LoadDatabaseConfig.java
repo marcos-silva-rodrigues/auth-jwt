f93d4cac0f3e482f56246fe7019ab4aada6ee949
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2024-04-24 19:15:55 -0300

    feat: dev and test profile with mysql db

--- src/main/java/com/silva/rodrigues/marcos/authjwt/config/LoadDatabaseConfig.java
+++ src/main/java/com/silva/rodrigues/marcos/authjwt/config/LoadDatabaseConfig.java
@@ -7,10 +7,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Configuration
+@Profile("!prod")
 public class LoadDatabaseConfig {
 
   private static Logger log = LoggerFactory.getLogger(LoadDatabaseConfig.class);
